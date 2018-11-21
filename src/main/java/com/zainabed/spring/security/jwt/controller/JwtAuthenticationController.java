3ae487f77a3610668f6b15ba90c081d5b0284a9e
Zainabed <zainabed@gmail.com>
2018-11-21 10:36:46 +0530

    Configured controller bean via condition.

--- src/main/java/com/zainabed/spring/security/jwt/controller/JwtAuthenticationController.java
+++ src/main/java/com/zainabed/spring/security/jwt/controller/JwtAuthenticationController.java
@@ -3,6 +3,7 @@ package com.zainabed.spring.security.jwt.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.zainabed.spring.security.jwt.service.JwtTokenService;
 
 @RestController
 @RequestMapping(value = "/auth")
+@ConditionalOnProperty(prefix = "jwt", name = "authentication")
 public class JwtAuthenticationController {
 
 	@Autowired
